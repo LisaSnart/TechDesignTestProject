88ba04f36d39e11294f7e4ecd2d1eda87b983900
LisaSnart <milyakova.katyunya@mail.ru>
2023-08-22 18:35:17 +0300

    Canvas setup
    Transition to the second stage, canvas setup, working with buttons

--- TechDesignTestProject/Assets/Code/Script/Load_Scene2.cs
+++ TechDesignTestProject/Assets/Code/Script/Load_Scene2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoaderScript : MonoBehaviour
+public class Load_Scene2 : MonoBehaviour
 {
     public void LoadSceneNumberOne()
     {
